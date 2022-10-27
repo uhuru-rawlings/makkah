565eab808c508635950368bd62c7d00f6545b1db
uhururawlings40 <uhururawlings40@gmail.com>
2022-10-27 22:13:26 +0300

    added php form handler

---
+++ bookings.php
@@ -0,0 +1,11 @@
+<?php
+    if(isset($_POST['locations'])){
+        $locations      = $_POST['locations'];
+        $fullname       = $_POST['fullname'];
+        $emailadress    = $_POST['emailadress'];
+        $phonenumber    = $_POST['phonenumber'];
+        $fromdate       = $_POST['fromdate'];
+        $todate         = $_POST['todate'];
+        $numberofpeople = $_POST['numberofpeople'];
+    }
+?>
---
+++ contact.php
@@ -0,0 +1,8 @@
+<?php
+    if(isset($_POST['phonenumber'])){
+        $fullname    = $_POST['fullname'];
+        $emailadress = $_POST['emailadress'];
+        $phonenumber = $_POST['phonenumber'];
+        $message     = $_POST['message'];
+    }
+?>
