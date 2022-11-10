7e8da3dde2b5f6fa45815a756d03a78276bc0e57
uhururawlings40 <uhururawlings40@gmail.com>
2022-11-10 09:05:22 +0300

    removed unwanted files

--- old-files/contact-fun.php
+++
@@ -1,8 +0,0 @@
-<?php
-    if(isset($_POST['phonenumber'])){
-        $fullname    = $_POST['fullname'];
-        $emailadress = $_POST['emailadress'];
-        $phonenumber = $_POST['phonenumber'];
-        $message     = $_POST['message'];
-    }
-?>
--- test/assets/js/google.js
+++
@@ -1,1 +0,0 @@
-https://saudiacdn.saudia.com/-/media/Base-Themes/Google-Maps-JS-Connector/Scripts/optimized-min.js?rev=e69959baa2ad4becbaa816f13f7c90b5&amp;t=20220718T081805Z&amp;hash=31443203F9B657B7785CAED56D0D2BDD
