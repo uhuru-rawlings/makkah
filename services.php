30a98b9b6f8221b7757647651dbca111df2af2bd
uhururawlings40 <uhururawlings40@gmail.com>
2022-11-15 18:12:23 +0300

    added services

--- services.php
+++ services.php
@@ -41,7 +41,7 @@
 	<div class="home">
 		<div class="home_background parallax-window" data-parallax="scroll" data-image-src="images/about_background.jpg"></div>
 		<div class="home_content">
-			<div class="home_title">our offers</div>
+			<div class="home_title">our services</div>
 		</div>
 	</div>
 
@@ -58,60 +58,7 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-lg-1 temp_col"></div>
-				<div class="col-lg-11">
-					
-					<!-- Offers Sorting -->
-					<div class="offers_sorting_container">
-						<ul class="offers_sorting">
-							<li>
-								<span class="sorting_text">price</span>
-								<i class="fa fa-chevron-down"></i>
-								<ul>
-									<li class="sort_btn" data-isotope-option='{ "sortBy": "original-order" }' data-parent=".price_sorting"><span>show all</span></li>
-									<li class="sort_btn" data-isotope-option='{ "sortBy": "price" }' data-parent=".price_sorting"><span>ascending</span></li>
-								</ul>
-							</li>
-							<li>
-								<span class="sorting_text">location</span>
-								<i class="fa fa-chevron-down"></i>
-								<ul>
-									<li class="sort_btn" data-isotope-option='{ "sortBy": "original-order" }'><span>default</span></li>
-									<li class="sort_btn" data-isotope-option='{ "sortBy": "name" }'><span>alphabetical</span></li>
-								</ul>
-							</li>
-							<li>
-								<span class="sorting_text">stars</span>
-								<i class="fa fa-chevron-down"></i>
-								<ul>
-									<li class="filter_btn" data-filter="*"><span>show all</span></li>
-									<li class="sort_btn" data-isotope-option='{ "sortBy": "stars" }'><span>ascending</span></li>
-									<li class="filter_btn" data-filter=".rating_3"><span>3</span></li>
-									<li class="filter_btn" data-filter=".rating_4"><span>4</span></li>
-									<li class="filter_btn" data-filter=".rating_5"><span>5</span></li>
-								</ul>
-							</li>
-							<li class="distance_item">
-								<span class="sorting_text">distance from center</span>
-								<i class="fa fa-chevron-down"></i>
-								<ul>
-									<li class="num_sorting_btn"><span>distance</span></li>
-									<li class="num_sorting_btn"><span>distance</span></li>
-									<li class="num_sorting_btn"><span>distance</span></li>
-								</ul>
-							</li>
-							<li>
-								<span class="sorting_text">reviews</span>
-								<i class="fa fa-chevron-down"></i>
-								<ul>
-									<li class="num_sorting_btn"><span>review</span></li>
-									<li class="num_sorting_btn"><span>review</span></li>
-									<li class="num_sorting_btn"><span>review</span></li>
-								</ul>
-							</li>
-						</ul>
-					</div>
-				</div>
-
+				
 				<div class="col-lg-12">
 					<!-- Offers Grid -->
 
@@ -126,12 +73,12 @@
 									<div class="offers_image_container">
 										<!-- Image by https://unsplash.com/@kensuarez -->
 										<div class="offers_image_background" style="background-image:url(images/offer_1.jpg)"></div>
-										<div class="offer_name"><a href="single_listing.html">grand castle</a></div>
+										<!-- <div class="offer_name"><a href="single_listing.html">grand castle</a></div> -->
 									</div>
 								</div>
 								<div class="col-lg-8">
 									<div class="offers_content">
-										<div class="offers_price">$70<span>per night</span></div>
+										<div class="offers_price">Hotel Booking</div>
 										<div class="rating_r rating_r_4 offers_rating" data-rating="4">
 											<i></i>
 											<i></i>
@@ -139,7 +86,9 @@
 											<i></i>
 											<i></i>
 										</div>
-										<p class="offers_text">Suspendisse potenti. In faucibus massa. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eu convallis tortor. Lorem ipsum dolor sit amet.</p>
+										<p class="offers_text">
+											We look for affordable,secure hotels who serves our customers needs and make them available to you.
+										</p>
 										<div class="offers_icons">
 											<ul class="offers_icons_list">
 												<li class="offers_icons_item"><img src="images/post.png" alt=""></li>
@@ -149,13 +98,6 @@
 											</ul>
 										</div>
 										<div class="button book_button"><a href="#">book<span></span><span></span><span></span></a></div>
-										<div class="offer_reviews">
-											<div class="offer_reviews_content">
-												<div class="offer_reviews_title">very good</div>
-												<div class="offer_reviews_subtitle">100 reviews</div>
-											</div>
-											<div class="offer_reviews_rating text-center">8.1</div>
-										</div>
 									</div>
 								</div>
 							</div>
@@ -169,13 +111,13 @@
 								<div class="col-lg-3 col-1680-4">
 									<div class="offers_image_container">
 										<!-- Image by https://unsplash.com/@thoughtcatalog -->
-										<div class="offers_image_background" style="background-image:url(images/offer_5.jpg)"></div>
+										<div class="offers_image_background" style="background-image:url(img/visa-assistance.jpg)"></div>
 										<div class="offer_name"><a href="single_listing.html">eurostar hotel</a></div>
 									</div>
 								</div>
 								<div class="col-lg-8">
 									<div class="offers_content">
-										<div class="offers_price">$50<span>per night</span></div>
+										<div class="offers_price">Visa Asistance</span></div>
 										<div class="rating_r rating_r_3 offers_rating" data-rating="3">
 											<i></i>
 											<i></i>
@@ -183,7 +125,9 @@
 											<i></i>
 											<i></i>
 										</div>
-										<p class="offers_text">Suspendisse potenti. In faucibus massa. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eu convallis tortor. Lorem ipsum dolor sit amet.</p>
+										<p class="offers_text">
+											We work hard to avail visa to our clients who need them, We are well connected and we can process your visa legaly but faster.
+										</p>
 										<div class="offers_icons">
 											<ul class="offers_icons_list">
 												<li class="offers_icons_item"><img src="images/post.png" alt=""></li>
@@ -193,13 +137,6 @@
 											</ul>
 										</div>
 										<div class="button book_button"><a href="#">book<span></span><span></span><span></span></a></div>
-										<div class="offer_reviews">
-											<div class="offer_reviews_content">
-												<div class="offer_reviews_title">very good</div>
-												<div class="offer_reviews_subtitle">100 reviews</div>
-											</div>
-											<div class="offer_reviews_rating text-center">8.1</div>
-										</div>
 									</div>
 								</div>
 							</div>
@@ -213,13 +150,13 @@
 								<div class="col-lg-3 col-1680-4">
 									<div class="offers_image_container">
 										<!-- Image by https://unsplash.com/@mindaugas -->
-										<div class="offers_image_background" style="background-image:url(images/offer_6.jpg)"></div>
+										<div class="offers_image_background" style="background-image:url(img/background-1.jpg)"></div>
 										<div class="offer_name"><a href="single_listing.html">grand castle</a></div>
 									</div>
 								</div>
 								<div class="col-lg-8">
 									<div class="offers_content">
-										<div class="offers_price">$110<span>per night</span></div>
+										<div class="offers_price">Airline Ticketing & Reservation</div>
 										<div class="rating_r rating_r_5 offers_rating"  data-rating="5">
 											<i></i>
 											<i></i>
@@ -227,7 +164,9 @@
 											<i></i>
 											<i></i>
 										</div>
-										<p class="offers_text">Suspendisse potenti. In faucibus massa. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eu convallis tortor. Lorem ipsum dolor sit amet.</p>
+										<p class="offers_text">
+											We are the experts in air ticketing in the region.You can take advantage of our partnership with leading local and international airlines to book your flights at the least cost and guar­antee hassle-free arrival to your desired destination. Our friendly customer agents are always available to assist with inquiries regarding our services and the flight you’re booked.
+										</p>
 										<div class="offers_icons">
 											<ul class="offers_icons_list">
 												<li class="offers_icons_item"><img src="images/post.png" alt=""></li>
@@ -237,13 +176,6 @@
 											</ul>
 										</div>
 										<div class="button book_button"><a href="#">book<span></span><span></span><span></span></a></div>
-										<div class="offer_reviews">
-											<div class="offer_reviews_content">
-												<div class="offer_reviews_title">very good</div>
-												<div class="offer_reviews_subtitle">100 reviews</div>
-											</div>
-											<div class="offer_reviews_rating text-center">8.1</div>
-										</div>
 									</div>
 								</div>
 							</div>
@@ -257,13 +189,13 @@
 								<div class="col-lg-3 col-1680-4">
 									<div class="offers_image_container">
 										<!-- Image by https://unsplash.com/@rktkn -->
-										<div class="offers_image_background" style="background-image:url(images/offer_7.jpg)"></div>
-										<div class="offer_name"><a href="single_listing.html">eurostar hotel</a></div>
+										<div class="offers_image_background" style="background-image:url(img/hajj-and-umrah.jpg)"></div>
+										<!-- <div class="offer_name"><a href="single_listing.html">eurostar hotel</a></div> -->
 									</div>
 								</div>
 								<div class="col-lg-8">
 									<div class="offers_content">
-										<div class="offers_price">$50<span>per night</span></div>
+										<div class="offers_price">Hajj & Umrah Services</div>
 										<div class="rating_r rating_r_4 offers_rating" data-rating="4">
 											<i></i>
 											<i></i>
@@ -271,7 +203,17 @@
 											<i></i>
 											<i></i>
 										</div>
-										<p class="offers_text">Suspendisse potenti. In faucibus massa. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eu convallis tortor. Lorem ipsum dolor sit amet.</p>
+										<p class="offers_text">
+											Driven by our deeply rooted origins and commitment to delivering the highest service standards, we have successfully distinguished ourselves in the world of hospitality by designing exceptional Hajj and Umrah packages and organizing Hajj and Umrah groups. We have consistently moved towards evolution and excellence in Hajj and Umrah services to ensure the best possible experience during the pilgrimage. 
+											<div class="row">
+												<div class="col-sm-6">
+													<a href="umra-services.php"><button class="btn btn-primary">Umrah Services</button></a>
+												</div>
+												<div class="col-sm-6">
+													<a href="hajj-services.php"><button class="btn btn-primary">Hajj Services</button></a>
+												</div>
+											</div>
+										</p>
 										<div class="offers_icons">
 											<ul class="offers_icons_list">
 												<li class="offers_icons_item"><img src="images/post.png" alt=""></li>
@@ -281,13 +223,6 @@
 											</ul>
 										</div>
 										<div class="button book_button"><a href="#">book<span></span><span></span><span></span></a></div>
-										<div class="offer_reviews">
-											<div class="offer_reviews_content">
-												<div class="offer_reviews_title">very good</div>
-												<div class="offer_reviews_subtitle">100 reviews</div>
-											</div>
-											<div class="offer_reviews_rating text-center">8.1</div>
-										</div>
 									</div>
 								</div>
 							</div>
@@ -301,13 +236,13 @@
 								<div class="col-lg-3 col-1680-4">
 									<div class="offers_image_container">
 										<!-- Image by https://unsplash.com/@itsnwa -->
-										<div class="offers_image_background" style="background-image:url(images/offer_8.jpg)"></div>
-										<div class="offer_name"><a href="single_listing.html">grand castle</a></div>
+										<div class="offers_image_background" style="background-image:url(img/surfing.jpg)"></div>
+										<!-- <div class="offer_name"><a href="single_listing.html">grand castle</a></div> -->
 									</div>
 								</div>
 								<div class="col-lg-8">
 									<div class="offers_content">
-										<div class="offers_price">$90<span>per night</span></div>
+										<div class="offers_price">Leisure and Holidays services</div>
 										<div class="rating_r rating_r_3 offers_rating" data-rating="3">
 											<i></i>
 											<i></i>
@@ -315,7 +250,9 @@
 											<i></i>
 											<i></i>
 										</div>
-										<p class="offers_text">Suspendisse potenti. In faucibus massa. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eu convallis tortor. Lorem ipsum dolor sit amet.</p>
+										<p class="offers_text">
+											Do you find it hard to choose the best holyday location for you or you and your family. We have sampled and chosen the best among the best Leisure and holiday destinations for you at an affordable price.
+										</p>
 										<div class="offers_icons">
 											<ul class="offers_icons_list">
 												<li class="offers_icons_item"><img src="images/post.png" alt=""></li>
@@ -325,13 +262,6 @@
 											</ul>
 										</div>
 										<div class="button book_button"><a href="#">book<span></span><span></span><span></span></a></div>
-										<div class="offer_reviews">
-											<div class="offer_reviews_content">
-												<div class="offer_reviews_title">very good</div>
-												<div class="offer_reviews_subtitle">100 reviews</div>
-											</div>
-											<div class="offer_reviews_rating text-center">8.1</div>
-										</div>
 									</div>
 								</div>
 							</div>
