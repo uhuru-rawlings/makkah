3aa898cd53b7e92dd42f5b4c752aa5fd0739f85e
Julius26 <juliusmeldrin@gmail.com>
2022-11-16 16:32:29 +0300

    updates: color changes

--- includes/footer.php
+++ includes/footer.php
@@ -10,14 +10,20 @@
 						<div class="footer_content footer_about">
 							<div class="logo_container footer_logo">
 								<div class="logo" style="width: 150px;"><a href="#">
-                                    <img src="images/logo-landscape.png" alt="" height="100%" width="100%" style="object-fit: cover;">
-                                </a></div>
+										<img src="images/logo-landscape.png" alt="" height="100%" width="100%" style="object-fit: cover;">
+									</a></div>
 							</div>
 							<p class="footer_about_text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus quis vu lputate eros, iaculis consequat nisl. Nunc et suscipit urna. Integer eleme ntum orci eu vehicula pretium.</p>
 							<ul class="footer_social_list">
-								<a href="#"><li class="footer_social_item"><i class="fa-brands fa-facebook-f"></i></li></a>
-								<a href="#"><li class="footer_social_item"><i class="fa-brands fa-twitter"></i></li></a>
-								<a href="#"><li class="footer_social_item"><i class="fa-brands fa-linkedin"></i></li></a>
+								<a href="#">
+									<li class="footer_social_item"><i class="fa-brands fa-facebook-f"></i></li>
+								</a>
+								<a href="#">
+									<li class="footer_social_item"><i class="fa-brands fa-twitter"></i></li>
+								</a>
+								<a href="#">
+									<li class="footer_social_item"><i class="fa-brands fa-linkedin"></i></li>
+								</a>
 							</ul>
 						</div>
 					</div>
@@ -50,19 +56,27 @@
 						<div class="footer_content footer_contact">
 							<ul class="contact_info_list">
 								<li class="contact_info_item d-flex flex-row">
-									<div><div class="contact_info_icon"><img src="images/placeholder.svg" alt=""></div></div>
+									<div>
+										<div class="contact_info_icon"><img src="images/placeholder.svg" alt=""></div>
+									</div>
 									<div class="contact_info_text">4127 Raoul Wallenber 45b-c Gibraltar</div>
 								</li>
 								<li class="contact_info_item d-flex flex-row">
-									<div><div class="contact_info_icon"><img src="images/phone-call.svg" alt=""></div></div>
+									<div>
+										<div class="contact_info_icon"><img src="images/phone-call.svg" alt=""></div>
+									</div>
 									<div class="contact_info_text"><a href="tel:+254 727 885454">+254 727 885454</a></div>
 								</li>
 								<li class="contact_info_item d-flex flex-row">
-									<div><div class="contact_info_icon"><img src="images/message.svg" alt=""></div></div>
+									<div>
+										<div class="contact_info_icon"><img src="images/message.svg" alt=""></div>
+									</div>
 									<div class="contact_info_text"><a href="mailto:info@alIhsanumrah.com" target="_top">info@alIhsanumrah.com</a></div>
 								</li>
 								<li class="contact_info_item d-flex flex-row">
-									<div><div class="contact_info_icon"><img src="images/planet-earth.svg" alt=""></div></div>
+									<div>
+										<div class="contact_info_icon"><img src="images/planet-earth.svg" alt=""></div>
+									</div>
 									<div class="contact_info_text"><a href="https://colorlib.com">www.colorlib.com</a></div>
 								</li>
 							</ul>
@@ -81,19 +95,23 @@
 			<div class="row">
 				<div class="col-lg-3 order-lg-1 order-2  ">
 					<div class="copyright_content d-flex flex-row align-items-center">
-						<div><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-						Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | Powered by: by <a href="http://fourtechglobalsolutions.com" target="_blank">Fourtech</a>
-						<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></div>
+						<div>
+							
+							Copyright &copy;<script>
+								document.write(new Date().getFullYear());
+							</script> All rights reserved | Powered by: <a href="http://fourtechglobalsolutions.com" target="_blank">Fourtech Global Solutions</a>
+							
+						</div>
 					</div>
 				</div>
 				<div class="col-lg-9 order-lg-2 order-1">
 					<div class="footer_nav_container d-flex flex-row align-items-center justify-content-lg-end">
 						<div class="footer_nav">
 							<ul class="footer_nav_list">
-								<li class="footer_nav_item"><a href="index.php">home</a></li>
-								<li class="footer_nav_item"><a href="about.php">about us</a></li>
+								<li class="footer_nav_item"><a href="index.php">Home</a></li>
+								<li class="footer_nav_item"><a href="about.php">About Us</a></li>
 								<li class="footer_nav_item"><a href="services.php">Services</a></li>
-								<li class="footer_nav_item"><a href="contact.php">contact</a></li>
+								<li class="footer_nav_item"><a href="contact.php">Contact</a></li>
 							</ul>
 						</div>
 					</div>
