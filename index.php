cae2139d3cfa9b1c6ab19f2a3d1e7c9c2f8ab22c
uhururawlings40 <uhururawlings40@gmail.com>
2022-11-18 15:59:50 +0300

    added image to featured

--- index.php
+++ index.php
@@ -1072,7 +1072,7 @@
 					<!-- Trending Item -->
 					<div class="col-lg-3 col-sm-6">
 						<div class="trending_item clearfix">
-							<div class="trending_image"><img src="images/trend_1.png" alt="https://unsplash.com/@fransaraco"></div>
+							<div class="trending_image"><img src="img/hotel-bookings.jpg" alt=""></div>
 							<div class="trending_content">
 								<div class="trending_title"><a href="#">grand hotel</a></div>
 								<div class="trending_price">From $182</div>
@@ -1083,7 +1083,7 @@
 					<!-- Trending Item -->
 					<div class="col-lg-3 col-sm-6">
 						<div class="trending_item clearfix">
-							<div class="trending_image"><img src="images/trend_2.png" alt="https://unsplash.com/@grovemade"></div>
+							<div class="trending_image"><img src="img/hotel-bookings.jpg" alt=""></div>
 							<div class="trending_content">
 								<div class="trending_title"><a href="#">mars hotel</a></div>
 								<div class="trending_price">From $182</div>
@@ -1094,7 +1094,7 @@
 					<!-- Trending Item -->
 					<div class="col-lg-3 col-sm-6">
 						<div class="trending_item clearfix">
-							<div class="trending_image"><img src="images/trend_3.png" alt="https://unsplash.com/@jbriscoe"></div>
+							<div class="trending_image"><img src="img/hotel-bookings.jpg" alt=""></div>
 							<div class="trending_content">
 								<div class="trending_title"><a href="#">queen hotel</a></div>
 								<div class="trending_price">From $182</div>
@@ -1105,7 +1105,7 @@
 					<!-- Trending Item -->
 					<div class="col-lg-3 col-sm-6">
 						<div class="trending_item clearfix">
-							<div class="trending_image"><img src="images/trend_4.png" alt="https://unsplash.com/@oowgnuj"></div>
+							<div class="trending_image"><img src="img/hotel-bookings.jpg" alt="https://unsplash.com/@oowgnuj"></div>
 							<div class="trending_content">
 								<div class="trending_title"><a href="#">mars hotel</a></div>
 								<div class="trending_price">From $182</div>
@@ -1116,7 +1116,7 @@
 					<!-- Trending Item -->
 					<div class="col-lg-3 col-sm-6">
 						<div class="trending_item clearfix">
-							<div class="trending_image"><img src="images/trend_5.png" alt="https://unsplash.com/@mindaugas"></div>
+							<div class="trending_image"><img src="img/hotel-bookings.jpg" alt="https://unsplash.com/@mindaugas"></div>
 							<div class="trending_content">
 								<div class="trending_title"><a href="#">grand hotel</a></div>
 								<div class="trending_price">From $182</div>
@@ -1127,7 +1127,7 @@
 					<!-- Trending Item -->
 					<div class="col-lg-3 col-sm-6">
 						<div class="trending_item clearfix">
-							<div class="trending_image"><img src="images/trend_6.png" alt="https://unsplash.com/@itsnwa"></div>
+							<div class="trending_image"><img src="img/hotel-bookings.jpg" alt="https://unsplash.com/@itsnwa"></div>
 							<div class="trending_content">
 								<div class="trending_title"><a href="#">mars hotel</a></div>
 								<div class="trending_price">From $182</div>
@@ -1139,7 +1139,7 @@
 					<!-- Trending Item -->
 					<div class="col-lg-3 col-sm-6">
 						<div class="trending_item clearfix">
-							<div class="trending_image"><img src="images/trend_7.png" alt="https://unsplash.com/@rktkn"></div>
+							<div class="trending_image"><img src="img/hotel-bookings.jpg" alt="https://unsplash.com/@rktkn"></div>
 							<div class="trending_content">
 								<div class="trending_title"><a href="#">queen hotel</a></div>
 								<div class="trending_price">From $182</div>
@@ -1151,7 +1151,7 @@
 					<!-- Trending Item -->
 					<div class="col-lg-3 col-sm-6">
 						<div class="trending_item clearfix">
-							<div class="trending_image"><img src="images/trend_8.png" alt="https://unsplash.com/@thoughtcatalog"></div>
+							<div class="trending_image"><img src="img/hotel-bookings.jpg" alt="https://unsplash.com/@thoughtcatalog"></div>
 							<div class="trending_content">
 								<div class="trending_title"><a href="#">mars hotel</a></div>
 								<div class="trending_price">From $182</div>
@@ -1163,7 +1163,7 @@
 				</div>
 				<div class="row">
 					<div class="col text-center">
-						<h2 class="section_title">Featured Packages</h2>
+						<h2 class="section_title">Testimonials</h2>
 					</div>
 				</div>
 				<div class="row py-4" id="testimonials">
